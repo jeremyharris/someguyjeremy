3840d949e2003a6c6b38de191ccf4a52aa1dc594
Jeremy Harris <jeremy@someguyjeremy.com>
2015-02-13 17:10:15 -0600

    Added cache busting

--- site/layout.php
+++ site/layout.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <title>someguyjeremy.com / <?php echo $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/styles.css">
-    <script src="/scripts.js"></script>
+    <link rel="stylesheet" href="/styles.css?_=<?php echo time(); ?>">
+    <script src="/scripts.js?_=<?php echo time(); ?>"></script>
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/styles/github.min.css">
     <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/highlight.min.js"></script>
     <script>hljs.initHighlightingOnLoad();</script>
