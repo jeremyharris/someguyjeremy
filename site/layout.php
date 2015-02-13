0ecc1f7da8b6451914c01b866dee6e93a6ba8f8b
Jeremy Harris <jeremy@someguyjeremy.com>
2015-02-12 18:20:58 -0600

    Added title to layout

--- site/layout.php
+++ site/layout.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>someguyjeremy.com</title>
+    <title>someguyjeremy.com / <?php echo $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/styles.css">
     <script src="/scripts.js"></script>
